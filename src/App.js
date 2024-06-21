a8f4f88c2ecb9a01b30fcb994838a6792d626ac7
yasharya2901 <yasharya2901@gmail.com>
2024-06-21 11:22:30 +0530

    First Commit: Learnt use of useCallback() function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useCallback} from 'react';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
+  const incrementCount = useCallback(() => setCount(count + 1), [count]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+        type = "text"
+        value = {input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={incrementCount}>Increment</button>
+      <h3>Input text: {input}</h3>
+      <h3>Count: {count}</h3>
+      <hr/>
+      <ChildComponent count={count} onClick={incrementCount}/>
     </div>
   );
 }
 
+
+const ChildComponent= React.memo(function({count, onClick}) {
+      console.log("Child component is rendering");;
+
+      return (
+        (<>
+          <div>
+            <h3>Count: {count}</h3>
+            <button onClick={onClick}>Increment</button>
+          </div>
+        </>)
+        );
+      }
+  )
 export default App;
